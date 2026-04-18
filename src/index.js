c2f7419e20effd09527d06f01cf7d6c87336f762
0x4rv1nd <arvind301024@gmail.com>
2026-04-19 00:39:40 +0530

    chore: update development server URL to include /api/v1 prefix

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 5000}`,
+        url: `http://localhost:${process.env.PORT || 5000}/api/v1`,
         description: 'Development server'
       }
     ],
